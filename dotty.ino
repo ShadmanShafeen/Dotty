68392648797f3a93b419b0d913d4a6afc4208ce0
Shadman Shafeen <shadmanskystar@gmail.com>
2024-07-20 21:09:57 +0600

    Modified clearLED function, and drastically reduced flicker

--- dotty.ino
+++ dotty.ino
@@ -1,118 +1,335 @@
-#include "MaxMatrix.h"
+#include <LedControl.h>
 
+#include <MD_Parola.h>     
+#include <MD_MAX72xx.h>    
+#include <SPI.h>  
 
-int DIN = 7;
-int CLK = 6;
-int CS = 5;
+// Pin assignments for the MAX7219
+const int dataIn = 7;  // Data In
+const int clk = 6;     // Clock
+const int cs = 5;      // Chip Select
+const int cs2 = 4;
 byte matNum = 1;
-
 String hDirection = "right";
 String vDirection = "up";
+String ballPath = "horizontal";
+int initialDelay = 50;
+int _delay = 100;
+const int ps = 3;
+int lip = 3;
+int rip = 3;
+int current32 = 1;  // 1 is the one with yellow cs pin
 
-byte buffer[20];
-char text[] = "a";
+int P1Score = 0;
+int P2Score = 0;
 
-MaxMatrix m(DIN, CS, CLK, matNum);
+#define JH1 A0
+#define JV1 A1
+#define JH2 A2
+#define JV2 A3
+
+// Create a new LedControl object
+LedControl lc = LedControl(dataIn, clk, cs, 4);  // (DataIn, CLK, CS, number of devices)
+LedControl lc2 = LedControl(dataIn, clk, cs2, 4);
 
 void setup() {
-  m.init();
-  m.setIntensity(5);
+
+  turnOnLeds();
+  Serial.begin(9600);
+  Serial.println("Start");
 }
 
 void loop() {
-  int x=0;
-  int y=0;
-  while(true)
-  {
-    if(hDirection == "right") {
-      if(x > 7) {
-        matNum = matNum+1;
-        m.setMatNum(matNum);
-        x=0;
+  int x = 4;
+  int y = 4;
+
+  while (true) {
+    // clear8x8DotMatrix(2, 0);
+    // delay(_delay);
+    // lightLED(2, 0, x, y);
+    int H1 = analogRead(JH1);
+    int V1 = analogRead(JV1);
+    int H2 = analogRead(JH2);
+    int V2 = analogRead(JV2);
+
+    // makeBothPaddles();
+    moveLeftBat(H1);
+    moveRightBat(H2);
+    checkPaddleHitPoint(x , y);
+    bool pointUpdated = checkIfPointScored(x, y);
+    if (pointUpdated) {
+      for (int i = 0; i <= 3; i++) {
+        clear8x8DotMatrix(current32, i);
       }
-      if(x == 6 && matNum == 4) {
-           hDirection = "left";
+      
+      lightLED(current32, matNum, x, y);
+      // makeBothPaddles();
+      delay(2000);
+      x = 4;
+      y = 0;
+      matNum = 1;
+      ballPath = "horizontal";
+    }
+    if (hDirection == "right") {
+      if (y < 0) {
+        matNum = matNum + 1;
+        y = 7;
+      }
+      if ((y == 1 && matNum == 3)) {
+        hDirection = "left";
       }
 
-      if(y == 7) {
+      if (x == 7) {
         vDirection = "down";
       }
-      if(y == 0) {
+      if (x == 0) {
         vDirection = "up";
       }
-      if(vDirection == "up") moveUpRight(x,y);
-      else moveDownRight(x,y);
+      if (ballPath == "diagonal") {
+        if (vDirection == "up") {
+          moveUpRight(x, y);
+        }
+        else {
+          moveDownRight(x, y);
+        }
+      }
+      else {
+        moveRight(x,y);
+      }
+      
     }
 
-    else if(hDirection == "left") {
-      if(x < 0) {
-        matNum = matNum-1;
-        m.setMatNum(matNum);
-        x=7;
+    else if (hDirection == "left") {
+      if (y > 7) {
+        matNum = matNum - 1;
+        y = 0;
       }
-      if(x == 1 && matNum == 1) {
-          hDirection = "right";
+      if (y == 6 && matNum == 0) {
+        hDirection = "right";
       }
 
-      if(y == 7) {
+      if (x == 7) {
         vDirection = "down";
       }
-      if(y == 0) {
+      if (x == 0) {
         vDirection = "up";
       }
-      if(vDirection == "up") moveUpLeft(x,y);
-      else moveDownLeft(x,y);
+      if (ballPath == "diagonal") {
+        if (vDirection == "up"){
+          moveUpLeft(x, y);
+        } 
+          else {
+          moveDownLeft(x, y);
+        } 
+      } 
+      else {
+        moveLeft(x,y);
+      }
+      
+    }
+
+    // clear8x8DotMatrix(current32, matNum);
+  }
+}
+
+void lightLED(int _ledNo, int matNum, int x, int y) {
+  if (_ledNo == 1) lc.setLed(matNum, x, y, true);
+  else lc2.setLed(matNum, x, y, true);
+}
+
+void clearLED(int _ledNo, int matNum , int x , int y) {
+  if (_ledNo == 1) lc.setLed(matNum, x, y, false);
+  else lc2.setLed(matNum, x, y, false); 
+}
+
+void clear8x8DotMatrix(int _ledNo, int matNum) {
+  if (_ledNo == 1) lc.clearDisplay(matNum);
+  else lc2.clearDisplay(matNum);
+}
+
+void moveRight(int& x, int& y)
+{
+  lightLED(current32, matNum, x, y);
+  delay(_delay);
+  clear8x8DotMatrix(current32, matNum);
+  --y;
+}
+
+void moveLeft(int& x, int& y)
+{
+  lightLED(current32, matNum, x, y);
+  delay(_delay);
+  clear8x8DotMatrix(current32, matNum);
+  ++y;
+}
+
+void moveUpRight(int& x, int& y) {
+  lightLED(current32, matNum, x, y);
+  delay(_delay);
+  clear8x8DotMatrix(current32, matNum);
+  --y;
+  ++x;
+}
+
+void moveDownRight(int& x, int& y) {
+  lightLED(current32, matNum, x, y);
+  delay(_delay);
+  clear8x8DotMatrix(current32, matNum);
+  --y;
+  --x;
+}
+
+void moveUpLeft(int& x, int& y) {
+  lightLED(current32, matNum, x, y);
+  delay(_delay);
+  clear8x8DotMatrix(current32, matNum);
+  ++y;
+  ++x;
+}
+
+void moveDownLeft(int& x, int& y) {
+  lightLED(current32, matNum, x, y);
+  delay(_delay);
+  clear8x8DotMatrix(current32, matNum);
+  ++y;
+  --x;
+}
+
+
+// void makeBothPaddles() {
+
+//   for (int i = lip; i < lip + ps; i++) {
+//     int temp = 7;
+//     lightLED(current32, 0, i, temp);
+//   }
+//   for (int i = rip; i < rip + ps; i++) {
+//     int temp = 0;
+//     lightLED(current32, 3, i, temp);
+//   }
+// }
+
+void moveLeftBat(int H1) {
+  if (H1 > 0 && H1 < 100) {
+    if (lip > 0) {
+      clearLED(current32 , 0 , lip + 2 , 7);
+      lip = lip - 1;
     }
   }
-  m.clear();
+  if (H1 > 650 && H1 < 1032) {
+    if (lip < 5) {
+      clearLED(current32 , 0 , lip , 7);
+      lip = lip + 1;
+    }
+  }
+  //  MAKE LEFT PADDLE
+  for (int i = lip; i < lip + ps; i++) {
+    int temp = 7;
+    lightLED(current32, 0, i, temp);
+  }
 }
 
-void moveRight(int& x, int y)
-{
-  m.setDot(y, x, true);  
-    delay(200);
-    m.clear();
-    ++x;
+void moveRightBat(int H2) {
+  if (H2 > 0 && H2 < 100) {
+    if (rip > 0) {
+      clearLED(current32 , 3 , rip + 2 , 0);
+      rip = rip - 1;
+    }
+  }
+  if (H2 > 650 && H2 < 1032) {
+    if (rip < 5) {
+      clearLED(current32 , 3 , rip , 0);
+      rip = rip + 1;
+    }
+  }
+  //  MAKE RIGHT PADDLE
+  for (int i = rip; i < rip + ps; i++) {
+    int temp = 0;
+    lightLED(current32, 3, i, temp);
+  }
 }
 
-void moveLeft(int& x, int y)
-{
-  m.setDot(y, x, true);
-    delay(200);
-    m.clear();
-    --x;
+bool checkIfPointScored(int x, int y) {
+  //P1 Score Update
+  int temp1 = P1Score, temp2 = P2Score;
+  if (matNum == 3 && y == 0 && (x < rip || x >= rip + ps)) {
+    P1Score++;
+  }
+  //P2 Score Update
+  if (matNum == 0 && y == 7 && (x < lip || x >= lip + ps)) {
+    P2Score++;
+  }
+  if (temp1 != P1Score) {
+    Serial.println("Player 1 Score: ");
+    Serial.println(P1Score);
+    return true;
+  }
+
+  if (temp2 != P2Score) {
+    Serial.println("Player 2 Score: ");
+    Serial.println(P2Score);
+    return true;
+  }
+
+  if (temp1 != P1Score || temp2 != P2Score) {
+    _delay = initialDelay;
+  }
+  return false;
 }
 
-void moveUpRight(int& x, int& y)
-{
-  m.setDot(y, x, true);
-    delay(200);
-    m.clear();
-    ++y;
-    ++x;
-}
-void moveDownRight(int& x, int& y)
-{
-  m.setDot(y, x, true);
-    delay(200);
-    m.clear();
-    --y;
-    ++x;
+void turnOnLeds() {
+  lc.shutdown(0, false);
+  lc.shutdown(1, false);
+  lc.shutdown(2, false);
+  lc.shutdown(3, false);
+
+  // lc2.shutdown(0, false);
+  // lc2.shutdown(1, false);
+  // lc2.shutdown(2, false);
+  // lc2.shutdown(3, false);
+
+  lc.setIntensity(0, 8);
+  lc.setIntensity(1, 8);
+  lc.setIntensity(2, 8);
+  lc.setIntensity(3, 8);
+  lc.clearDisplay(0);
+
+  // lc2.setIntensity(0, 8);
+  // lc2.clearDisplay(0);
 }
 
-void moveUpLeft(int& x, int& y)
-{
-  m.setDot(y, x, true);
-    delay(200);
-    m.clear();
-    ++y;
-    --x;
+void printGoal() {
+
 }
-void moveDownLeft(int& x, int& y)
-{
-  m.setDot(y, x, true);
-    delay(200);
-    m.clear();
-    --y;
-    --x;
+
+void checkPaddleHitPoint(int x , int y) {
+  if ( (matNum == 3 && y == 0 && (x >= rip || x < rip + ps))  ) {
+    if ((x == rip + 1)) {
+      ballPath = "horizontal";
+    }
+    else {
+      ballPath = "diagonal";
+      if(x == rip + 2) {
+        vDirection = "up";
+      }
+      else if (x == rip) {
+        vDirection = "down";
+      }
+    }
+    _delay--;
+  }
+  if ((matNum == 0 && y == 7 && (x >= lip || x < lip + ps))) {
+    if ((x == lip + 1)) {
+      ballPath = "horizontal";
+    }
+    else {
+      ballPath = "diagonal";
+      if(x == lip + 2) {
+        vDirection = "up";
+      }
+      else if (x == lip) {
+        vDirection = "down";
+      }
+    }
+    _delay--;
+  }
 }
